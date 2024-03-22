7e105d39b3912960dd5e8bb407d9addc85b57cb0
LusineKarapetyann <lusinekarapetyan013@gmail.com>
2024-03-22 20:21:01 +0400

    ps3

--- DependencyInjection.Core/AcaServiceCollection.cs
+++ DependencyInjection.Core/AcaServiceCollection.cs
@@ -6,7 +6,7 @@ public class AcaServiceCollection : IAcaServiceCollection
     {
         throw new NotImplementedException();
     }
-
+     
     public IAcaServiceProvider Build()
     {
         throw new NotImplementedException();
